a290ff892c8f7ca62466c865bec78537734e88eb
missed86 <jimmy11286@gmail.com>
2023-02-06 08:23:00 +0100

    Proyecto terminado

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse     # new
+
 
 # Create your views here.
+def home_page_view(request):             # new
+    return HttpResponse("¡Hola mundo!")  # new
